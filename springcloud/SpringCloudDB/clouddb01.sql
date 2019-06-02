256e938335e7144c03d3d8c14d939d57100030ac
郑嘉伟 <1048549613@qq.com>
2019-06-02 09:42:30 +0800

    添加数据库文件

---
+++ springcloud/SpringCloudDB/clouddb01.sql
@@ -0,0 +1,40 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : ZhengJiaWei
+ Source Server Type    : MySQL
+ Source Server Version : 50643
+ Source Host           : localhost:3306
+ Source Schema         : clouddb01
+
+ Target Server Type    : MySQL
+ Target Server Version : 50643
+ File Encoding         : 65001
+
+ Date: 02/06/2019 09:40:59
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for dept
+-- ----------------------------
+DROP TABLE IF EXISTS `dept`;
+CREATE TABLE `dept`  (
+  `deptno` int(11) NOT NULL AUTO_INCREMENT,
+  `dname` varchar(60) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `db_source` varchar(60) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`deptno`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Records of dept
+-- ----------------------------
+INSERT INTO `dept` VALUES (1, '开发部', 'clouddb01');
+INSERT INTO `dept` VALUES (2, '人事部', 'clouddb01');
+INSERT INTO `dept` VALUES (3, '财务部', 'clouddb01');
+INSERT INTO `dept` VALUES (4, '市场部', 'clouddb01');
+INSERT INTO `dept` VALUES (5, '运维部', 'clouddb01');
+
+SET FOREIGN_KEY_CHECKS = 1;
